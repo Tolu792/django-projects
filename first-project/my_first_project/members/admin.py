46a11c59d9c3a09435689c116dd75fbbe57c7efa
TOLUWANIMI <ttgbadebo@sidmach.com>
2024-06-05 12:03:01 +0100

    new commit

--- first-project/my_first_project/members/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Member
-
-# Register your models here.
-
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "joined_date", "phone")
-
-admin.site.register(Member, MemberAdmin)
